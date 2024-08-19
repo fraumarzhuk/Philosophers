d0c2df45478c8c4a0696bfa84b8a3fdf1a92d665
Marianna Zhukova <mzhukova@c2r5s8.42wolfsburg.de>
2024-08-19 14:59:36 +0200

    small fix

--- src/set_env.c
+++ src/set_env.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 16:06:28 by mariannazhu       #+#    #+#             */
-/*   Updated: 2024/08/19 14:52:48 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/08/19 14:59:12 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ void	create_threads(t_env *philo_info, t_philo *philos)
 		pthread_mutex_unlock(&philo_info->mutex);
 		philos[i].ate_times = 0;
 		philos[i].index = i;
-		philos[i].time_last_meal = get_current_time();
+		philos[i].time_last_meal = philo_info->time_begin;
 		// philos[i]. time_begin = philos[i].time_last_meal;
 		error = pthread_create(&philos[i].thread, NULL, life_cycle, &philos[i]);
 		if (error != 0)
